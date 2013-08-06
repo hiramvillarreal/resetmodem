e320a9aba81ed51d95257162b202a727127bc487
Niarfe <efrain.olivares@gmail.com>
2013-08-06 05:15:24 +0000

    Replace tabs with spaces in order to reduce potential unwanted behavior because of nesting rules in python.

--- test_connection.py
+++ test_connection.py
@@ -7,8 +7,8 @@ def cknet():
         response=urllib2.urlopen("http://google.com")
         print "Successfully opened URL"
     except urllib2.URLError as err:
-    	print "We have no connection"
-	pass
+        print "We have no connection"
+        pass
 
 if __name__ == "__main__":
     cknet()
