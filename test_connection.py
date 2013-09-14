5f9d5b01107b47b5e1cd9cd59003a5b99ef3149a
n6egy <jdhmd@cox.net>
2013-09-14 11:37:54 -0700

    Set up logic for email flag.  Added debugging logic as well.

--- test_connection.py
+++ test_connection.py
@@ -1,8 +1,30 @@
 import urllib2
 import gpiooutput
 import sys
+
+
+retries = 1
+
 def cknet():
+    global retries
+    debug = 0
+    fake_status = 1
+
     print "Testing for internet connectivity"
+
+    # debugging vv
+    if debug == 1:
+       print "DEBUG"
+       if retries == 1:
+           print "DEBUG succeed case"
+           retries = 0
+           return 1
+       else:
+           print "DEBUG fail case"
+           return 0
+       #return fake_status
+     # debugging ^^
+
     try:
         response=urllib2.urlopen("http://www.msftncsi.com/ncsi.txt")
         print "Successfully opened URL"
@@ -11,12 +33,23 @@ def cknet():
         print "We have no connection"
         gpiooutput.toggle()
         return 1
-if __name__ == "__main__":
-    for i in range(3):
-        print "Toggle Routine"
-        if 0 == cknet():
-            print "Success"
-            sys.exit()
 
-    
-    
+
+if __name__ == "__main__":
+    send_email = 0
+
+    for i in range(3):
+        status = cknet()
+        print "Toggle Routine"
+        if status == 0:
+            print "Success"
+            if send_email == 1:
+                print "Detect previous failure, send email with log"
+            break
+
+        else:
+            print "Failed to find connection, Retrying"    
+            send_email = 1
+
+#    if send_email == 1:
+#        print "We would have command to send email here"
