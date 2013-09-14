10501d5b9083fa3c5feb35265218bc0613e1dc12
n6egy <jdhmd@cox.net>
2013-09-14 12:29:24 -0700

    Changed commenting but nothing in the script itself.

--- gpiooutput.py
+++ gpiooutput.py
@@ -1,10 +1,9 @@
-##    LED BLINKER   ##
-#  LED Blinker on Pin 11 (Stolen from "Raspberry Pi User Guide" pg 194)
-#  Ver. 0.1
-#  August 5, 2013
-#  J. Hobson
-#  Just prior to this, I imported "raspberry-gpio-python"library  (ibid pg 190)
-
+# Python Script
+# Name: gpiooutput.py
+# Version: 0.5
+# Author:  Efrain Olivares
+# Date: Sept 14, 2013
+# Function:  Power cycles Cox cable modem when called by another script, test_connectioon.py.
 
 import RPi.GPIO as GPIO
 import time
@@ -13,12 +12,12 @@ import time
 
 def toggle():    
 
-    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT) 		# Selects GPIO pin 11 on RPi
     for i in range(1):
-        GPIO.output(11, True)
-        time.sleep(15)
-        GPIO.output(11, False)
-        time.sleep(240)
+        GPIO.output(11, True)           # Pin 11 goes high, opening a NC relay and cutting power to modem
+        time.sleep(15)                  # Keeps power off for 15 sec to allow modem to "die"
+        GPIO.output(11, False)          # Pin 11 goes low to re-power the modem
+        time.sleep(240)                 # 4 minute delay to allow modem to re-establish connection to Cox DOCSIS
 
 
            
