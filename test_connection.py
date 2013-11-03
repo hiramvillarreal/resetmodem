ee784a00f5b21981bd008401f9db09bf22791a95
Niarfe <efrain.olivares@gmail.com>
2013-11-02 21:03:42 -0700

    Add documentation strings, comments
    Add documentation strings in python standard, """ string after definition.  http://www.python.org/dev/peps/pep-0008/#comments
    
    Add same line comments for debug flag, fake status

--- test_connection.py
+++ test_connection.py
@@ -9,13 +9,21 @@ logging.basicConfig(filename='example.log', level=logging.DEBUG)
 retries = 1
 
 def cknet():
+    """Check Internet Connection
+    
+    This function is called to probe for an internet connection.  It uses a try/except 
+    block to attempt to open a connection to a known stable internet site.  If it succeeds
+    it will return false, else if an exception occurs, it will return false.
+    
+    """
+    
     global retries
-    debug = 0
-    fake_status = 0
+    debug = 0        # used for debugging only, bypasses internet check
+    fake_status = 0  # used for debugging, simulate success or fail for internet check
 
     print_debug("Testing for internet connectivity")
 
-    # debugging vv
+    # The following block is used for debugging only.
     if debug == 1:
        print "DEBUG"
        if retries == 1:
@@ -38,12 +46,31 @@ def cknet():
         return 1
 
 def print_debug( _string, _print_to_console=1 ):
+    """Add timestamp and write debugging to file.
+    
+    This function makes use of a timestamp and uses the logging package to write
+    to a log.  Wrapping this functionality allows us to change format in one location
+    and avoid duplicated code.
+    
+    """
+    
     if _print_to_console == 1:
         print str( datetime.datetime.now() ) + ":  " + _string
     logging.debug( str( datetime.datetime.now() ) + ": " +  _string )
 
 
 if __name__ == "__main__":
+    """Main running loop.
+    
+    The send_mail flag is set when we do not detect a connection.  Once we have estabished 
+    internet connectivity it triggers the email alert.
+    
+    We loop three times, checking for internet connectivity, exiting on success, or retrying.
+    
+    This script is set on a cron job which runs every 10 minutes.
+    
+    """
+    
     send_email = 0
 
     for i in range(3):
